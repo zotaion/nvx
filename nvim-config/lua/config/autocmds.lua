ed54c2d64598fe327346d08a16efc37d01706769
ion.zota <ion.v.zota@gmail.com>
2025-11-12 10:14:39 +0200

    lsp

--- nvim-config/lua/config/autocmds.lua
+++ nvim-config/lua/config/autocmds.lua
@@ -5,3 +5,16 @@ vim.cmd [[
         autocmd FileType java lua require'config.jdtls'.setup_jdtls()
     augroup end
 ]]
+
+-- Debug command to show diagnostics with source information
+vim.api.nvim_create_user_command('ShowDiagnostics', function()
+    local diagnostics = vim.diagnostic.get(0)
+    for _, diag in ipairs(diagnostics) do
+        print(string.format(
+            "Code: %s, Source: %s, Message: %s",
+            diag.code or "nil",
+            diag.source or "nil",
+            diag.message
+        ))
+    end
+end, {})
--- nvim-config/lua/plugins/lsp-config.lua
+++ nvim-config/lua/plugins/lsp-config.lua
@@ -1,133 +1,160 @@
 -- LSP configuration (Mason setup kept for compatibility but LSP servers provided by Nix)
 do
-    -- setup mason with default properties (for manual package management if needed)
-    require("mason").setup({
-        ui = {
-            border = "rounded"
-        }
-    })
+	-- setup mason with default properties (for manual package management if needed)
+	require("mason").setup({
+		ui = {
+			border = "rounded",
+		},
+	})
 
-    -- mason lsp config setup (LSP servers provided by Nix PATH)
-    require("mason-lspconfig").setup({
-        ensure_installed = {},  -- Empty, Nix provides all servers: lua_ls, ts_ls, jdtls, cssls, zls, html, eslint, angularls
-        automatic_installation = false,
-    })
+	-- mason lsp config setup (LSP servers provided by Nix PATH)
+	require("mason-lspconfig").setup({
+		ensure_installed = {}, -- Empty, Nix provides all servers: lua_ls, ts_ls, jdtls, cssls, zls, html, eslint, angularls
+		automatic_installation = false,
+	})
 
-    -- mason nvim dap setup (debug adapters provided by Nix)
-    require("mason-nvim-dap").setup({
-        ensure_installed = {},  -- Empty, Nix provides adapters
-        automatic_installation = false,
-    })
+	-- mason nvim dap setup (debug adapters provided by Nix)
+	require("mason-nvim-dap").setup({
+		ensure_installed = {}, -- Empty, Nix provides adapters
+		automatic_installation = false,
+	})
 
-    -- lsp signature setup
-    require("lsp_signature").setup()
+	-- lsp signature setup
+	require("lsp_signature").setup()
 end
 
 -- nvim-lspconfig setup
 do
-    local icons = require("config.icons")
+	local icons = require("config.icons")
 
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    -- Add general capabilities offset encoding
-    capabilities.general = capabilities.general or {}
-    capabilities.general.positionEncodings = { "utf-16", "utf-8" }
+	local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	-- Add general capabilities offset encoding
+	capabilities.general = capabilities.general or {}
+	capabilities.general.positionEncodings = { "utf-16", "utf-8" }
 
-    -- Setup LSP servers using vim.lsp.config (new API for Neovim 0.11+)
-    local lspconfig = require('lspconfig')
-    local util = require('lspconfig.util')
+	-- Setup LSP servers using vim.lsp.config (new API for Neovim 0.11+)
+	local lspconfig = require("lspconfig")
+	local util = require("lspconfig.util")
 
-    local servers = {
-        lua_ls = {},
-        ts_ls = {},
-        cssls = {},
-        zls = {},
-        html = {
-            -- Don't start in Angular projects - let angularls handle it
-            root_dir = function(fname)
-                local angular_root = util.root_pattern('angular.json', '.git')(fname)
-                if angular_root then
-                    return nil  -- Don't start html LS in Angular projects
-                end
-                return util.root_pattern('package.json', '.git')(fname)
-            end,
-            -- Disable diagnostics to avoid false positives
-            handlers = {
-                ["textDocument/publishDiagnostics"] = function() end,
-            },
-        },
-        eslint = {},
-    }
+	local servers = {
+		lua_ls = {},
+		ts_ls = {
+			-- Explicitly exclude HTML files - let angularls or html handle them
+			filetypes = {
+				"javascript",
+				"javascriptreact",
+				"javascript.jsx",
+				"typescript",
+				"typescriptreact",
+				"typescript.tsx",
+			},
+			-- Prevent ts_ls from starting in HTML files, even in Angular projects
+			root_dir = function(fname)
+				-- Never attach to HTML files
+				if fname:match("%.html$") then
+					return nil
+				end
+				return util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git")(fname)
+			end,
+		},
+		cssls = {},
+		zls = {},
+		html = {
+			-- Don't start in Angular projects - let angularls handle it
+			root_dir = function(fname)
+				local angular_root = util.root_pattern("angular.json")(fname)
+				if angular_root then
+					return nil -- Don't start html LS in Angular projects
+				end
+				return util.root_pattern("package.json", ".git")(fname)
+			end,
+		},
+		eslint = {},
+	}
+	-- Setup each server using the proper API
+	for server, config in pairs(servers) do
+		config.capabilities = vim.tbl_deep_extend("force", {}, capabilities, config.capabilities or {})
+		lspconfig[server].setup(config)
+	end
 
-    -- Setup each server using the proper API
-    for server, config in pairs(servers) do
-        config.capabilities = vim.tbl_deep_extend('force', {}, capabilities, config.capabilities or {})
-        lspconfig[server].setup(config)
-    end
+	-- Angular language server with better diagnostic filtering
+	lspconfig.angularls.setup({
+		capabilities = capabilities,
+		cmd = {
+			"ngserver",
+			"--stdio",
+			"--tsProbeLocations",
+			vim.fn.getcwd(),
+			"--ngProbeLocations",
+			vim.fn.getcwd(),
+		},
+		filetypes = { "typescript", "html", "typescripthtml", "typescript.tsx" },
+		root_dir = util.root_pattern("angular.json", "project.json"),
 
-    -- Angular language server
-    lspconfig.angularls.setup({
-        capabilities = capabilities,
-        -- Reduce false positive diagnostics in templates
-        init_options = {
-            -- Disable strict template checking to reduce false positives
-            strictTemplates = false,
-        },
-        -- Disable diagnostics from angularls, keep only navigation/completion
-        handlers = {
-            ["textDocument/publishDiagnostics"] = function() end,
-        },
-    })
+		-- Filter diagnostics instead of disabling them completely
+		handlers = {
+			["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+				-- Filter out common false positives
+				virtual_text = true,
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+			}),
+		},
 
+		on_attach = function(client, bufnr)
+			-- Optional: disable specific capabilities if needed
+			-- client.server_capabilities.documentFormattingProvider = false
+		end,
+	})
 
-            local default_diagnostic_config = {
-                signs = {
-                    text = {
-                        [vim.diagnostic.severity.ERROR] = icons.diagnostics.Error,
-                        [vim.diagnostic.severity.WARN] = icons.diagnostics.Warning,
-                        [vim.diagnostic.severity.HINT] = icons.diagnostics.Hint,
-                        [vim.diagnostic.severity.INFO] = icons.diagnostics.Information,
-                    },
-                },
-                virtual_text = false,
-                update_in_insert = false,
-                underline = true,
-                severity_sort = true,
-                float = {
-                    focusable = true,
-                    style = "minimal",
-                    border = "rounded",
-                    source = "always",
-                    header = "",
-                    prefix = "",
-                },
-            }
+	local default_diagnostic_config = {
+		signs = {
+			text = {
+				[vim.diagnostic.severity.ERROR] = icons.diagnostics.Error,
+				[vim.diagnostic.severity.WARN] = icons.diagnostics.Warning,
+				[vim.diagnostic.severity.HINT] = icons.diagnostics.Hint,
+				[vim.diagnostic.severity.INFO] = icons.diagnostics.Information,
+			},
+		},
+		virtual_text = false,
+		update_in_insert = false,
+		underline = true,
+		severity_sort = true,
+		float = {
+			focusable = true,
+			style = "minimal",
+			border = "rounded",
+			source = "always",
+			header = "",
+			prefix = "",
+		},
+	}
 
-            vim.diagnostic.config(default_diagnostic_config)
+	vim.diagnostic.config(default_diagnostic_config)
 
-
-
-            -- Set vim motion for <Space> + c + h to show code documentation about the code the cursor is currently over if available
-            vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over Documentation" })
-            -- Set vim motion for <Space> + c + d to go where the code/variable under the cursor was defined
-            vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "[C]ode Goto [D]efinition" })
-            -- Set vim motion for <Space> + c + a for display code action suggestions for code diagnostics in both normal and visual mode
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
-            -- Set vim motion for <Space> + c + r to display references to the code under the cursor
-            vim.keymap.set(
-                "n",
-                "<leader>cr",
-                require("telescope.builtin").lsp_references,
-                { desc = "[C]ode Goto [R]eferences" }
-            )
-            -- Set vim motion for <Space> + c + i to display implementations to the code under the cursor
-            vim.keymap.set(
-                "n",
-                "<leader>ci",
-                require("telescope.builtin").lsp_implementations,
-                { desc = "[C]ode Goto [I]mplementations" }
-            )
-            -- Set a vim motion for <Space> + c + <Shift>R to smartly rename the code under the cursor
-            vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, { desc = "[C]ode [R]ename" })
-    -- Set a vim motion for <Space> + c + <Shift>D to go to where the code/object was declared in the project (class file)
-    vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "[C]ode Goto [D]eclaration" })
+	-- Set vim motion for <Space> + c + h to show code documentation about the code the cursor is currently over if available
+	vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over Documentation" })
+	-- Set vim motion for <Space> + c + d to go where the code/variable under the cursor was defined
+	vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "[C]ode Goto [D]efinition" })
+	-- Set vim motion for <Space> + c + a for display code action suggestions for code diagnostics in both normal and visual mode
+	vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
+	-- Set vim motion for <Space> + c + r to display references to the code under the cursor
+	vim.keymap.set(
+		"n",
+		"<leader>cr",
+		require("telescope.builtin").lsp_references,
+		{ desc = "[C]ode Goto [R]eferences" }
+	)
+	-- Set vim motion for <Space> + c + i to display implementations to the code under the cursor
+	vim.keymap.set(
+		"n",
+		"<leader>ci",
+		require("telescope.builtin").lsp_implementations,
+		{ desc = "[C]ode Goto [I]mplementations" }
+	)
+	-- Set a vim motion for <Space> + c + <Shift>R to smartly rename the code under the cursor
+	vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, { desc = "[C]ode [R]ename" })
+	-- Set a vim motion for <Space> + c + <Shift>D to go to where the code/object was declared in the project (class file)
+	vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "[C]ode Goto [D]eclaration" })
 end
